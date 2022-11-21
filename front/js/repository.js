769b4387d50e67d23bf07ee6a2b0853637702b48
Antoine Labard <antoine@labard.fr>
2022-11-21 08:58:05 +0100

    feat: ✨ add repository.js with getAllProducts function

---
+++ front/js/repository.js
@@ -0,0 +1,15 @@
+async function getAllProducts() {
+    fetch("http://localhost:3000/api/products")
+        .then(function (res) {
+            if (res.ok) {
+                return res.json()
+            }
+        })
+        .then(function (value) {
+            console.log(value)
+        })
+        .catch(function (err) {
+            console.log(err)
+            return {}
+        });
+}
