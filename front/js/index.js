b060ccca6c1638287c9e28d2e3cf9945c4d563e7
Antoine Labard <antoine@labard.fr>
2022-11-22 10:38:10 +0100

    feat: ✨ implement the products list in the main page

---
+++ front/js/index.js
@@ -0,0 +1,22 @@
+import Repository from "./repository.js"
+
+const repository = new Repository()
+let items = document.getElementById('items')
+let productsHTML = "" // The string containing all the HTML element describing the products.
+
+repository.getAllProducts().then(function (products) {
+    console.log(products)
+    for (let i in products) {
+        let product = products[i]
+        console.log(product)
+        productsHTML = productsHTML + `
+        <a href="./product.html?id=${product._id}">
+            <article>
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>
+            </article>
+        </a>`
+    }
+    items.innerHTML = productsHTML
+})
