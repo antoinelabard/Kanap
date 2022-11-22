c882fdb7fd896bf56b71a56db3fac0f8eb262514
Antoine Labard <antoine@labard.fr>
2022-11-22 20:13:37 +0100

    feat: ✨ implement the functionality to add an item to the cart

--- front/js/product.js
+++ front/js/product.js
@@ -22,4 +22,19 @@ repository
             <option value="${product.colors[i]}">${product.colors[i]}</option>`
         }
         colorsHTML.innerHTML += colorOptions
+
+        document.getElementById("addToCart").addEventListener("click", (event) => {
+            let color = document.getElementById("colors").value
+            if (!color) {
+                alert("Veuillez choisir la couleur de votre article.")
+                return
+            }
+            let quantity = Number(document.getElementById("quantity").value)
+            if (quantity < 1 || quantity > 100) {
+                alert("Le nombre d'articles doit être compris entre 1 et 100.")
+                return
+            }
+            repository.addToCart(product._id, color, quantity)
+            alert("Le produit a été ajouté au panier !")
+        })
     })
