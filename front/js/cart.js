2edfd3cb6405d797c6cc32ae9e46aa85f3bd61bf
Antoine Labard <antoine@labard.fr>
2022-11-23 20:08:44 +0100

    refactor: 🎨 put all the code in cart.js in functions
    This make it more reusable and avoid having to reload the page at each change.

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,41 +7,87 @@ let totalQuantityHTML = document.getElementById("totalQuantity")
 let totalPriceHTML = document.getElementById("totalPrice")
 
 let cartItems = repository.getCart()
+let products = {}
 
 let totalQuantity = 0
 let totalPrice = 0
 
-for (let i in cartItems) {
-  let cartItem = cartItems[i]
-  await repository
-    .getProductById(cartItem.id)
-    .then((product) => {
-      itemsSectionHTML.innerHTML += `
-            <article class="cart__item" data-id="${product._id}" data-color="${cartItem.color}">
-                <div class="cart__item__img">
-                  <img src="${product.imageUrl}" alt="${product.altTxt}">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${product.name}</h2>
-                    <p>${cartItem.color}</p>
-                    <p>${product.price} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartItem.quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article>`
-      totalQuantity += cartItem.quantity
-      totalPrice += product.price * cartItem.quantity
+function addItemCardToDom(cartItem) {
+  let product = products[cartItem.id]
+  let itemHTML = document.createElement("item")
+  itemHTML.innerHTML = `
+    <article class="cart__item" data-id="${product._id}" data-color="${cartItem.color}">
+        <div class="cart__item__img">
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+        </div>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${product.name}</h2>
+            <p>${cartItem.color}</p>
+            <p>${product.price} €</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté : </p>
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartItem.quantity}">
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p class="deleteItem">Supprimer</p>
+            </div>
+          </div>
+        </div>
+      </article>`
+  itemHTML.getElementsByClassName("deleteItem")[0]
+    .addEventListener("click", () => {
+      console.log(cartItem.color)
+      repository.removeFromCart(cartItem.id, cartItem.color)
+      itemsSectionHTML.removeChild(itemHTML)
     })
+  itemHTML.getElementsByClassName("itemQuantity")[0]
+    .addEventListener("change", (value) => {
+      console.log(event.target.value)
+      repository.addToCart(cartItem.id, cartItem.color, event.target.value)
+    })
+  itemsSectionHTML.appendChild(itemHTML)
 }
 
-totalQuantityHTML.textContent = totalQuantity
-totalPriceHTML.textContent = totalPrice
+function setUpListeners() {
+  for (let i in itemsSectionHTML.children) {
+    itemHTML = itemsSectionHTML.children[i]
+    console.log(itemHTML.getAttribute)
+    itemsSectionHTML.lastChild.addEventListener("click", () => {
+      repository.removeFromCart(item.id, item.color)
+      itemsSectionHTML.removeChild(itemsSectionHTML.lastChild)
+    })
+    itemsSectionHTML.lastChild.addEventListener("change", (value) => {
+      console.log("quantity changed")
+      repository.addToCart(item.id, item.color, event.target.value)
+    })
+  }
+}
+
+function updateTotals() {
+  totalQuantity = cartItems.reduce((acc, item) => {
+    return acc + item.quantity
+  })
+  totalPrice = cartItems.reduce((acc, item) => {
+    return acc + item.quantity * item.price
+  })
+  totalQuantityHTML.textContent = totalQuantity
+  totalPriceHTML.textContent = totalPrice
+}
+
+async function loadProductsData() {
+  for (let i in cartItems) {
+    let cartItem = cartItems[i]
+    await repository
+      .getProductById(cartItem.id)
+      .then((product) => {
+        products[product._id] = product
+        addItemCardToDom(cartItem)
+        updateTotals()
+      })
+  }
+}
+
+loadProductsData()
